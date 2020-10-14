b34bcabb7837dfe32201f84f5d54167e2546f287
Victor Moreno <victor.mordur@gmail.com>
2020-10-14 21:15:39 +0200

    Cleanup serialization madness

--- data-donation-sdk/src/androidMain/kotlin/care/data4life/datadonation/encryption/hybrid/HybridEncryptionAndroidSerializer.kt
+++ data-donation-sdk/src/androidMain/kotlin/care/data4life/datadonation/encryption/hybrid/HybridEncryptionAndroidSerializer.kt
@@ -32,15 +32,15 @@
 
 package care.data4life.datadonation.encryption.hybrid
 
-import io.ktor.utils.io.bits.*
-import io.ktor.utils.io.core.*
-import io.ktor.utils.io.core.internal.*
-import java.nio.ByteBuffer
 import care.data4life.datadonation.encryption.hybrid.HybridEncryptionPayload.Companion.AES_IV_LENGTH
 import care.data4life.datadonation.encryption.hybrid.HybridEncryptionPayload.Companion.AES_IV_SIZE_LENGTH
 import care.data4life.datadonation.encryption.hybrid.HybridEncryptionPayload.Companion.AES_KEY_LENGTH
 import care.data4life.datadonation.encryption.hybrid.HybridEncryptionPayload.Companion.CIPHERTEXT_SIZE_LENGTH
 import care.data4life.datadonation.encryption.hybrid.HybridEncryptionPayload.Companion.VERSION_LENGTH
+import io.ktor.utils.io.bits.*
+import io.ktor.utils.io.core.*
+import io.ktor.utils.io.core.internal.*
+import java.nio.ByteBuffer
 
 internal actual val hybridEncryptionSerializer: HybridEncryptionPayload.Serializer
     get() = HybridEncryptionAndroidSerializer
@@ -59,11 +59,17 @@ internal object HybridEncryptionAndroidSerializer : HybridEncryptionPayload.Seri
         val wBuffer = Buffer(Memory(outputByteBuffer))
 
         payload.apply {
+            //version
             wBuffer.writeUByte(version.toUByte())
+            //encKeyLen
             wBuffer.writeUShort(encryptedSymmetricPrivateKey.size.toUShort())
+            //encKey
             wBuffer.writeFully(encryptedSymmetricPrivateKey)
+            //iv
             wBuffer.writeFully(iv)
+            //ciphertextLen
             wBuffer.writeULong(ciphertext.size.toULong()) // TODO check if it needs to be written as little endian
+            //ciphertext
             wBuffer.writeFully(ciphertext)
         }
 
@@ -79,32 +85,39 @@ internal object HybridEncryptionAndroidSerializer : HybridEncryptionPayload.Seri
         if (data.size <= FIXED_OUTPUT_LENGTH) {
             throw IllegalArgumentException("Input data too short")
         }
-        val versionBytes =  ByteArray(VERSION_LENGTH)
+        //version
+        val versionBytes = ByteArray(VERSION_LENGTH)
         val versionBuffer = Buffer(Memory(ByteBuffer.wrap(versionBytes)))
         versionBuffer.resetForRead()
         val version = versionBuffer.readUByte().toInt()
 
+        //encKey
+        val keyPos = VERSION_LENGTH + AES_IV_SIZE_LENGTH
+        val aesEncryptedKey = ByteArray(AES_KEY_LENGTH)
+        data.copyInto(aesEncryptedKey, 0, keyPos, keyPos + AES_KEY_LENGTH)
+
+        //IV
+        val ivPos = keyPos + AES_KEY_LENGTH
+        val iv = ByteArray(AES_IV_LENGTH)
+        data.copyInto(iv, 0, ivPos, ivPos + AES_IV_LENGTH)
+
+        //ciphertextLen
         val ciphertextSizeBytes = ByteArray(CIPHERTEXT_SIZE_LENGTH)
         val cipherTextPos = FIXED_OUTPUT_LENGTH
         data.copyInto(ciphertextSizeBytes, 0, cipherTextPos - CIPHERTEXT_SIZE_LENGTH, cipherTextPos)
 
         val sizeBuffer = Buffer(Memory(ByteBuffer.wrap(ciphertextSizeBytes)))
         sizeBuffer.resetForRead()
-        val  b=  ByteArray(8)
+        val b = ByteArray(CIPHERTEXT_SIZE_LENGTH)
         sizeBuffer.readAvailable(b)
         b.reverse()
         val k = Buffer(Memory(ByteBuffer.wrap(b)))
         k.resetForRead()
         val ciphertextSize = k.readULong().toInt()
 
-        val iv = ByteArray(AES_IV_LENGTH)
+        //ciphertext
         val ciphertext = ByteArray(ciphertextSize)
-        val keyPos = VERSION_LENGTH + AES_IV_SIZE_LENGTH
-        val ivPos = keyPos + AES_KEY_LENGTH
-        data.copyInto(iv, 0, ivPos, ivPos + AES_IV_LENGTH)
         data.copyInto(ciphertext, 0, cipherTextPos, cipherTextPos + ciphertextSize)
-        val aesEncryptedKey = ByteArray(AES_KEY_LENGTH)
-        data.copyInto(aesEncryptedKey, 0, keyPos, keyPos + AES_KEY_LENGTH)
 
         return HybridEncryptionPayload(aesEncryptedKey, iv, ciphertext, version)
     }
