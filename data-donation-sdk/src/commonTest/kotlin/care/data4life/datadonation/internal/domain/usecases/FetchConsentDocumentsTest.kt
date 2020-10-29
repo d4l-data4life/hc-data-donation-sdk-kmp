81c1e323b6fc1a643ca0c03fc4f36ae58c859eac
Victor Moreno <victor.mordur@gmail.com>
2020-10-29 19:09:38 +0100

    Uncomment usecase unit tests so Android tests can be successfully run from a clean stash.

--- data-donation-sdk/src/commonTest/kotlin/care/data4life/datadonation/internal/domain/usecases/FetchConsentDocumentsTest.kt
+++ data-donation-sdk/src/commonTest/kotlin/care/data4life/datadonation/internal/domain/usecases/FetchConsentDocumentsTest.kt
@@ -28,7 +28,7 @@
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *//*
+ */
 
 
 package care.data4life.datadonation.internal.domain.usecases
@@ -69,5 +69,3 @@ abstract class FetchConsentDocumentsTest {
     class FetchConsentDocumentListener: CapturingResultListener<List<ConsentDocument>>()
 }
 
-
-*/
