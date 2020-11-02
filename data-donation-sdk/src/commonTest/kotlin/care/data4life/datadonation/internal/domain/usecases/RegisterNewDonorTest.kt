82095a0a6b841bdeeebb4ae251c1abb583b9c29d
stumi01 <stumiw@gmail.com>
2020-11-02 07:45:34 +0100

    Fix tests.

--- data-donation-sdk/src/commonTest/kotlin/care/data4life/datadonation/internal/domain/usecases/RegisterNewDonorTest.kt
+++ data-donation-sdk/src/commonTest/kotlin/care/data4life/datadonation/internal/domain/usecases/RegisterNewDonorTest.kt
@@ -28,7 +28,7 @@
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *//*
+ */
 
 
 package care.data4life.datadonation.internal.domain.usecases
@@ -156,4 +156,3 @@ abstract class RegisterNewDonorTest {
     class RegisterNewDonorListener: CapturingResultListener<KeyPair>()
 
 }
-*/
