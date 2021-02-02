34a9dc21083cecf029013ebec9155cee06fc7d05
stumi01 <stumiw@gmail.com>
2021-02-02 10:41:21 +0100

    Add FilterSensitiveInformation usecase.

--- data-donation-sdk/src/commonTest/kotlin/care/data4life/datadonation/internal/domain/mock/MockFilterSensitiveInformation.kt
+++ data-donation-sdk/src/commonTest/kotlin/care/data4life/datadonation/internal/domain/mock/MockFilterSensitiveInformation.kt
@@ -1,7 +1,7 @@
 /*
  * BSD 3-Clause License
  *
- * Copyright (c) 2020, D4L data4life gGmbH
+ * Copyright (c) 2021, D4L data4life gGmbH
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,32 +30,13 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package care.data4life.datadonation.internal.domain.usecases
+package care.data4life.datadonation.internal.domain.mock
 
-import org.junit.Ignore
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
+import care.data4life.datadonation.internal.domain.usecases.FilterSensitiveInformation
+import care.data4life.fhir.stu3.model.FhirResource
 
-@RunWith(JUnit4::class)
-class FetchUserConsentsAndroidTest : FetchUserConsentsTest()
-
-@RunWith(JUnit4::class)
-class CreateUserConsentAndroidTest : CreateUserConsentTest()
-
-@RunWith(JUnit4::class)
-class FetchConsentAndroidDocumentsTest : FetchConsentDocumentsTest()
-
-@RunWith(JUnit4::class)
-class FilterSensitiveInformationAndroidTest : FilterSensitiveInformationTest()
-
-@RunWith(JUnit4::class)
-class RegisterNewDonorAndroidTest : RegisterNewDonorTest()
-
-@RunWith(JUnit4::class)
-class RevokeUserConsentAndroidTest : RevokeUserConsentTest()
-
-@Ignore // TODO: Ignored until fhir kmp dependency is updated to parse FhirResource to json
-@RunWith(JUnit4::class)
-class DonateResourcesAndroidTest : DonateResourcesTest()
-
-
+class MockFilterSensitiveInformation : FilterSensitiveInformation() {
+    override suspend fun execute(): List<FhirResource> {
+        return parameter
+    }
+}
