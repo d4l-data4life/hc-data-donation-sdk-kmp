3d45598b99bdf4fc1286ffeba94ae7bc72f10bef
stumi01 <stumiw@gmail.com>
2021-07-02 19:41:56 +0200

    Add toByteArray import to remove ambiguity.

--- data-donation-sdk/src/iosMain/kotlin/care/data4life/datadonation/encryption/symmetric/EncryptionSymmetricKeyNative.kt
+++ data-donation-sdk/src/iosMain/kotlin/care/data4life/datadonation/encryption/symmetric/EncryptionSymmetricKeyNative.kt
@@ -32,6 +32,7 @@
 
 package care.data4life.datadonation.encryption.symmetric
 
+import care.data4life.datadonation.toByteArray
 import care.data4life.datadonation.toNSData
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.memScoped
@@ -60,11 +61,11 @@ class EncryptionSymmetricKeyNative : EncryptionSymmetricKey {
         runCatching {
             val iv = encrypted.sliceArray(0..15)
             val encrypted = encrypted.sliceArray(16..encrypted.lastIndex)
-            return@runCatching cryptoWrapper.decryptWithEncrypted(
+            return@runCatching (cryptoWrapper.decryptWithEncrypted(
                 encrypted.toNSData(),
                 iv.toNSData(),
                 associatedData.toNSData()
-            ).toByteArray()
+            ) as NSData).toByteArray()
         }
 
     override fun serialized(): ByteArray = key
@@ -74,11 +75,11 @@ class EncryptionSymmetricKeyNative : EncryptionSymmetricKey {
 
     override fun encrypt(plainText: ByteArray, associatedData: ByteArray): ByteArray = memScoped {
         val iv = secureRandomByteArray(16)
-        val encrypted = cryptoWrapper.encryptWithPlainText(
+        val encrypted = (cryptoWrapper.encryptWithPlainText(
             plainText.toNSData(),
             iv.toNSData(),
             associatedData.toNSData()
-        ).toByteArray()
+        ) as NSData).toByteArray()
         if (encrypted.isEmpty()) {
             throw Throwable("Unknown encryption error")
         }
