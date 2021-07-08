5f99953be534ba930b19ab69230ad251a05e6b64
Matthias Geisler <matthias.geisler@data4life.care>
2021-07-08 06:43:09 +0200

    Refactor DocumentWithSignature and DonationPayload

---
+++ data-donation-sdk/src/commonMain/kotlin/care/data4life/datadonation/internal/data/model/DocumentWithSignature.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021 D4L data4life gGmbH / All rights reserved.
+ *
+ * D4L owns all legal rights, title and interest in and to the Software Development Kit ("SDK"),
+ * including any intellectual property rights that subsist in the SDK.
+ *
+ * The SDK and its documentation may be accessed and used for viewing/review purposes only.
+ * Any usage of the SDK for other purposes, including usage for the development of
+ * applications/third-party applications shall require the conclusion of a license agreement
+ * between you and D4L.
+ *
+ * If you are interested in licensing the SDK for your own applications/third-party
+ * applications and/or if you’d like to contribute to the development of the SDK, please
+ * contact D4L by email to help@data4life.care.
+ */
+
+package care.data4life.datadonation.internal.data.model
+
+data class DocumentWithSignature(val document: ByteArray, val signature: ByteArray) {
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            !is DocumentWithSignature -> false
+            else -> document.contentEquals(other.document) && signature.contentEquals(other.signature)
+        }
+    }
+
+    override fun hashCode(): Int {
+        var result = document.contentHashCode()
+        result = 31 * result + signature.contentHashCode()
+        return result
+    }
+}
