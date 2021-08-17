e1f9d151fbadaaaf3caf510238b68cdb0044ac4c
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-08-17 15:31:57 +0200

    Adds xcodeproject to for crypto

---
+++ DataDonationCrypto-ObjC/DataDonationCrypto-ObjC/DataDonationCrypto_ObjC.h
@@ -0,0 +1,18 @@
+//
+//  DataDonationCrypto_ObjC.h
+//  DataDonationCrypto-ObjC
+//
+//  Created by Alessio Borraccino on 17.08.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataDonationCrypto_ObjC.
+FOUNDATION_EXPORT double DataDonationCrypto_ObjCVersionNumber;
+
+//! Project version string for DataDonationCrypto_ObjC.
+FOUNDATION_EXPORT const unsigned char DataDonationCrypto_ObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataDonationCrypto_ObjC/PublicHeader.h>
+
+
---
+++ DataDonationCrypto-ObjC/DataDonationCrypto-ObjC/DataDonationCryptor.swift
@@ -0,0 +1,20 @@
+//
+//  DataDonationCryptor.swift
+//  DataDonationCrypto-ObjC
+//
+//  Created by Alessio Borraccino on 17.08.21.
+//
+
+import Foundation
+import Data4LifeCrypto
+
+@objc final class DataDonationCryptor: NSObject {
+
+    @objc static func encrypt(_ data: Data, using key: Data) -> Data {
+        return data
+    }
+
+    @objc static func decrypt(_ data: Data, using key: Data) -> Data {
+        return data
+    }
+}
