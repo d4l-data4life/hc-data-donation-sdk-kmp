6dc1c6b7e1c0c07549e4cb451989f93b8c5d6324
Matthias Geisler <matthias.geisler@data4life.care>
2021-08-28 14:01:51 +0200

    Add DonorKeyStorageKoin

---
+++ data-donation-sdk/src/commonTest/kotlin/care/data4life/datadonation/donation/donorkeystorage/DonorKeyStorageKoinTest.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2021 D4L data4life gGmbH / All rights reserved.
+ *
+ * D4L owns all legal rights, title and interest in and to the Software Development Kit ("SDK"),
+ * including any intellectual property rights that subsist in the SDK.
+ *
+ * The SDK and its documentation may be accessed and used for viewing/review purposes only.
+ * Any usage of the SDK for other purposes, including usage for the development of
+ * applications/third-party applications shall require the conclusion of a license agreement
+ * between you and D4L.
+ *
+ * If you are interested in licensing the SDK for your own applications/third-party
+ * applications and/or if you’d like to contribute to the development of the SDK, please
+ * contact D4L by email to help@data4life.care.
+ */
+
+package care.data4life.datadonation.donation.donorkeystorage
+
+import care.data4life.datadonation.DataDonationSDK
+import care.data4life.datadonation.mock.stub.donation.donorkeystorage.DonorKeyStorageProviderStub
+import care.data4life.sdk.util.coroutine.CoroutineScopeFactory
+import kotlinx.coroutines.CoroutineScope
+import org.koin.core.context.stopKoin
+import org.koin.dsl.koinApplication
+import org.koin.dsl.module
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class DonorKeyStorageKoinTest {
+    private val testScope = CoroutineScopeFactory.createScope("test2Scope")
+
+    @BeforeTest
+    fun setUp() {
+        stopKoin()
+    }
+
+    @Test
+    fun `Given resolveDonorKeyStorageKoinModule is called it creates a Module, which contains a DonorKeyStorageRepository`() {
+        // When
+        val koin = koinApplication {
+            modules(
+                resolveDonorKeyStorageKoinModule(),
+                module {
+                    single<DataDonationSDK.DonorKeyStorageProvider> {
+                        DonorKeyStorageProviderStub()
+                    }
+                    single<CoroutineScope> { testScope }
+                }
+            )
+        }
+        // Then
+        val builder: DonorKeyStorageRepositoryContract = koin.koin.get()
+        assertNotNull(builder)
+    }
+}
