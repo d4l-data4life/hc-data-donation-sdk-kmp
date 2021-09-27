eec6addc38103a5a568b06811a8ee60cedce8186
Matthias Geisler <matthias.geisler@data4life.care>
2021-09-27 15:59:22 +0200

    Add QuestionnaireResponseItemListIterator

---
+++ data-donation-sdk/src/commonMain/kotlin/care/data4life/datadonation/donation/fhir/wrapper/QuestionnaireResponseItemListIterator.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021 D4L data4life gGmbH / All rights reserved.
+ *
+ * D4L owns all legal rights, title and interest in and to the Software Development Kit ("SDK"),
+ * including any intellectual property rights that subsist in the SDK.
+ *
+ * The SDK and its documentation may be accessed and used for viewing/review purposes only.
+ * Any usage of the SDK for other purposes, including usage for the development of
+ * applications/third-party applications shall require the conclusion of a license agreement
+ * between you and D4L.
+ *
+ * If you are interested in licensing the SDK for your own applications/third-party
+ * applications and/or if you’d like to contribute to the development of the SDK, please
+ * contact D4L by email to help@data4life.care.
+ */
+
+package care.data4life.datadonation.donation.fhir.wrapper
+
+import care.data4life.datadonation.donation.fhir.wrapper.CompatibilityWrapperContract.QuestionnaireResponseItem
+
+data class Fhir3QuestionnaireResponseItemListIteratorWrapper(
+    private val iterator: ListIterator<Fhir3QuestionnaireResponseItem>
+) : CompatibilityWrapperContract.QuestionnaireResponseItemListIterator<Fhir3QuestionnaireResponseItem, Fhir3QuestionnaireResponseItemAnswer, Fhir3DateTime> {
+    override fun hasNext(): Boolean {
+        return iterator.hasNext()
+    }
+
+    override fun hasPrevious(): Boolean {
+        return iterator.hasPrevious()
+    }
+
+    override fun next(): QuestionnaireResponseItem<Fhir3QuestionnaireResponseItem, Fhir3QuestionnaireResponseItemAnswer, Fhir3DateTime> {
+        return Fhir3QuestionnaireResponseItemWrapper(iterator.next())
+    }
+
+    override fun nextIndex(): Int {
+        return iterator.nextIndex()
+    }
+
+    override fun previous(): QuestionnaireResponseItem<Fhir3QuestionnaireResponseItem, Fhir3QuestionnaireResponseItemAnswer, Fhir3DateTime> {
+        return Fhir3QuestionnaireResponseItemWrapper(iterator.previous())
+    }
+
+    override fun previousIndex(): Int {
+        return iterator.previousIndex()
+    }
+
+    override fun unwrap(): ListIterator<Fhir3QuestionnaireResponseItem> {
+        return iterator
+    }
+}
+
+data class Fhir4QuestionnaireResponseItemListIteratorWrapper(
+    private val iterator: ListIterator<Fhir4QuestionnaireResponseItem>
+) : CompatibilityWrapperContract.QuestionnaireResponseItemListIterator<Fhir4QuestionnaireResponseItem, Fhir4QuestionnaireResponseItemAnswer, Fhir4DateTime> {
+    override fun hasNext(): Boolean {
+        return iterator.hasNext()
+    }
+
+    override fun hasPrevious(): Boolean {
+        return iterator.hasPrevious()
+    }
+
+    override fun next(): QuestionnaireResponseItem<Fhir4QuestionnaireResponseItem, Fhir4QuestionnaireResponseItemAnswer, Fhir4DateTime> {
+        return Fhir4QuestionnaireResponseItemWrapper(iterator.next())
+    }
+
+    override fun nextIndex(): Int {
+        return iterator.nextIndex()
+    }
+
+    override fun previous(): QuestionnaireResponseItem<Fhir4QuestionnaireResponseItem, Fhir4QuestionnaireResponseItemAnswer, Fhir4DateTime> {
+        return Fhir4QuestionnaireResponseItemWrapper(iterator.previous())
+    }
+
+    override fun previousIndex(): Int {
+        return iterator.previousIndex()
+    }
+
+    override fun unwrap(): ListIterator<Fhir4QuestionnaireResponseItem> {
+        return iterator
+    }
+}
